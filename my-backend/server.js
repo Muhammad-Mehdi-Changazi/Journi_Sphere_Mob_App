a4d5c4bd8f7e4168c82ce5a62bc8bf995bb32120
Muhammad Mehdi Changezi <113912243+Muhammad-Mehdi-Changazi@users.noreply.github.com>
2024-11-10 23:35:31 +0500

    env file added

--- my-backend/server.js
+++ my-backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const Routes = require('./routes/routes');
 const mongoose = require('mongoose');
+require('dotenv').config();  // Import dotenv to load environment variables
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,15 +11,13 @@ const port = process.env.PORT || 3000;
 app.use(cors({ origin: '*', credentials: true }));
 app.use(bodyParser.json());
 
-//Connecting mongoose
-mongoose.connect('mongodb+srv://sQpbJkHNcJzho6Pd:sQpbJkHNcJzho6Pd@manzil.gxdiu.mongodb.net/', 
-    { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log('Error connecting to MongoDB:', err));
-
+// Connecting mongoose using the MongoDB URI from the .env file
+mongoose.connect(process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('Error connecting to MongoDB:', err));
 
 app.use('/', Routes);
-
 
 // Start the server
 app.listen(port, () => {
