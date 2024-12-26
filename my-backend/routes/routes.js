41a516b9b40022e2a8c2b15df858e0992ace8844
Umer-IK <umerinayat20@gmail.com>
2024-12-26 07:58:40 +0500

    models, controllers, and routing for hotels and rooms

--- my-backend/routes/routes.js
+++ my-backend/routes/routes.js
@@ -3,6 +3,9 @@ const authController = require('../controllers/authController');
 const reviewController = require('../controllers/reviewController');
 const cityController = require('../controllers/cityController')
 const recommendationController = require('../controllers/recommendationController')
+const { createHotel, getHotels, getHotelByName } = require('../controllers/hotelController');
+const { createRoom, getRooms, getRoomsByHotel } = require('../controllers/roomController');
+
 
 const router = express.Router();
 router.post('/signup', authController.signup);
@@ -11,5 +14,12 @@ router.get('/Reviews', reviewController.getReviews);
 router.post('/Reviews', reviewController.createReview);
 router.get('/api/cities', cityController.getCities);
 router.get('/recommendations', recommendationController.getRecommendations)
+router.post('/', createHotel);
+router.get('/', getHotels);
+router.get('/:hotel_name', getHotelByName);
+router.post('/', createRoom);
+router.get('/', getRooms);
+router.get('/hotel/:hotel_name', getRoomsByHotel);
+
 
 module.exports = router;
