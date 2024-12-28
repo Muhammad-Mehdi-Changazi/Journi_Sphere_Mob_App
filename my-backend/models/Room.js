5e50b5c72d232e7d3c0bda6435fdae1fee9e2237
Shahrexz <shahrez.faisal89@gmail.com>
2024-12-28 15:07:59 +0500

    hotel

--- my-backend/models/Room.js
+++ my-backend/models/Room.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
+  room_type: { type: String, required: true },
   price: { type: Number, required: true },
   available: { type: Boolean, required: true },
-  room_type: { type: String, required: true },
-  duplicates: { type: Number, required: true }, // how many rooms there are of a certain type
-  hotel_name: { type: String, ref: 'Hotel', required: true },
-  num_booked: { type: Number, required: true }, // how many of those have been booked.
-
+  duplicates: { type: Number, required: true }, // Number of rooms available for this type
+  num_booked: { type: Number, required: true }, // Number of booked rooms
+  hotel: { type: mongoose.Schema.Types.ObjectId, ref: 'Hotel', required: true }, // Reference to Hotel schema
 });
 
 module.exports = mongoose.model('Room', roomSchema);
